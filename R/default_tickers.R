ddb3955d6333aa24d8df3177d8c14e9b4110e75e
Martin Frigaard <mjfrigaard@pm.me>
2026-04-16 11:02:54 -0700

    updates to pkgdown

--- R/default_tickers.R
+++ R/default_tickers.R
@@ -1,6 +1,6 @@
 #' Default stock tickers available in the app
 #'
-#' A character vector of mega-cap tickers used as app defaults.
+#' A character vector of pharma company tickers used as app defaults.
 #'
 #' @export
-default_tickers <- c("AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA")
+default_tickers <- c("LLY", "MRK", "JNJ", "PFE", "ABBV", "BMY", "AMGN")
